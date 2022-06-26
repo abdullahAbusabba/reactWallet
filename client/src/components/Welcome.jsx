b5ec57c484f524d33b8659f6d94521e501e9d12c
abdullahAbusabba <alajmi7abdullah@gmail.com>
2022-06-26 11:42:37 +0300

    feat:

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,8 +1,11 @@
 import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
+import { Card } from "./";
+
 import { Loader } from "./";
 const Welcome = () => {
+  const handleSubmit = () => {};
   const commonStyles =
     "min-h-[70px] sm:px-0  px-2  flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white hover:text-black hover:bg-white";
   const Input = ({ placeholder, name, type, value, handleChange }) => (
@@ -39,14 +42,7 @@ const Welcome = () => {
           >
             <p className="text-white text-base font-semibold">Connect Wallet</p>
           </button>
-          <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10 ">
-            <div className={`rounded-tl-2xl ${commonStyles}`}>Reliability</div>
-            <div className={commonStyles}>Security</div>
-            <div className={`sm:rounded-tr-2xl ${commonStyles}`}>Ethereum</div>
-            <div className={`sm:rounded-bl-2xl ${commonStyles}`}>Web 3.0</div>
-            <div className={commonStyles}>Low Fees</div>
-            <div className={`rounded-br-2xl ${commonStyles}`}>Blockchain</div>
-          </div>
+          <Card />
         </div>
         <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
           <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
@@ -71,6 +67,17 @@ const Welcome = () => {
             <Input placeholder="Keyword (Gif)" name="keyword" type="text" />
             <Input placeholder="Enter Message" name="message" type="text" />
             <div className="h-[1px] w-full bg-gray-400 my-2" />
+            {false ? (
+              <Loader />
+            ) : (
+              <button
+                type="button"
+                onClick={handleSubmit}
+                className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer hover:bg-[#d0d1d4] hover:text-black hover:border-black"
+              >
+                Send Now
+              </button>
+            )}
           </div>
         </div>
       </div>
